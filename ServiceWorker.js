85181e756677ddd4c888115a3a5b557b79778e5b
ADMIN-PC\Admin <nguyenthaibao3303@gmail.com>
2026-05-22 18:11:02 +0700

    Deploy 35

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "DefaultCompany-Kickoff Rise Trader-2.0.11";
+const CACHE_NAME = "DefaultCompany-Kickoff Rise Trader-2.0.12";
 
 const contentToCache = [
   "Build/KickoffRiseTrader.WebGL.loader.js",
